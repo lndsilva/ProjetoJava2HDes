7fa4d81b6a9fd1c9b966aeac5367511bc0b67bf4
Laercio-pc <lndsilva@hotmail.com>
2020-06-23 22:31:43 -0300

    banco de dados parte 1

---
+++ ProjetoBancoDados/src/PacoteBancoDados/Conexao.java
@@ -0,0 +1,52 @@
+package PacoteBancoDados;
+
+import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Conexao {
+
+    public static Connection abrirConexao() {
+
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            String url = "jdbc:mysql://127.0.0.1/estacionamento?user=root&password=";
+
+            con = DriverManager.getConnection(url);
+
+            System.out.println("Conexão aberta...");
+
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+
+        } catch (ClassNotFoundException ex) {
+            
+            System.out.println(ex.getMessage());
+            
+        } catch (Exception ex) {
+            
+            System.out.println(ex.getMessage());
+
+        }
+
+        return con;
+
+    }
+    public static void fecharConexao(Connection con){
+        
+        try {
+            con.close();
+            System.out.println("Conexão Fechada...");
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+            
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        
+    }
+
+}
